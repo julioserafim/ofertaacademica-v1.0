8c4a359eee08bd87ecaab8f39af5310feefe0844
MaikeBezerra <maike_ti@hotmail.com>
2020-05-29 19:44:11 -0300

    removendo feature envy professor isCoordenação

--- ofertaacademica-v1.0/aplicacao/oferta-academica/src/main/java/ufc/quixada/npi/ap/model/Papel.java
+++ ofertaacademica-v1.0/aplicacao/oferta-academica/src/main/java/ufc/quixada/npi/ap/model/Papel.java
@@ -64,6 +64,10 @@ public class Papel implements GrantedAuthority {
 		}
 	}
 
+	public boolean papelCoordenacao() {
+		return this.nome == Tipo.COORDENACAO;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
